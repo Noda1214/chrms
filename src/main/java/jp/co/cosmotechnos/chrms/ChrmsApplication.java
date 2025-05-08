834aaa61866ef75dadfa9c83bd2bdbdc71a11441
Noda Rintarou <rintarou.noda@cosmotechnos.co.jp>
2025-05-08 18:05:22 +0900

    Initial commit

---
+++ src/main/java/jp/co/cosmotechnos/chrms/ChrmsApplication.java
@@ -0,0 +1,13 @@
+package jp.co.cosmotechnos.chrms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChrmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChrmsApplication.class, args);
+	}
+
+}
